7900a0a0357ce10f0f31d429649a1392ed497ab2
wangxun521 <70557037+wangxun521@users.noreply.github.com>
2024-07-02 20:30:48 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -29,7 +29,6 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 # ------------------------------- Other started -------------------------------
 #
 # Add luci-app-amlogic
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 svn co https://github.com/xiaorouji/openwrt-passwall/tree/main/luci-app-passwall package/luci-app-passwall
 svn co https://github.com/vernesong/OpenClash/tree/master/luci-app-openclash package/luci-app-openclash
 # Fix runc version error
